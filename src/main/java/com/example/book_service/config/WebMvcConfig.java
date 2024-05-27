ed1efb88250be678babbe7ee888ce64c68b415be
jisoo <zlsoo9ee@gmail.com>
2024-05-27 12:56:19 +0900

    UPDATE FastAPI CORS config

--- src/main/java/com/example/book_service/config/WebMvcConfig.java
+++ src/main/java/com/example/book_service/config/WebMvcConfig.java
@@ -11,9 +11,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     /* CORS 설정 추가*/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        
+        // React CORS 설정
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 적용
-                .allowedOrigins("http://43.203.38.124") // 허용할 Origin 설정
+                .allowedOrigins("http://43.203.38.124") // react 애플리케이션의 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
+                .allowCredentials(true);    // 자격 증명 허용 여부 설정
+
+        // FastAPI CORS 설정
+        registry.addMapping("/**")
+                .allowedOrigins("http://43.200.64.238") // FastAPI 애플리케이션의 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")// 허용할 HTTP 메서드 설정
                 .allowCredentials(true);    // 자격 증명 허용 여부 설정
     }
 
