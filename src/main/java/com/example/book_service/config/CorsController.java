5d8a49aa93ffdbdf45031084b703c8b1e53d2138
jisoo <zlsoo9ee@gmail.com>
2024-05-28 23:03:11 +0900

    test

--- src/main/java/com/example/book_service/config/CorsController.java
+++ src/main/java/com/example/book_service/config/CorsController.java
@@ -1,14 +1,14 @@
-package com.example.book_service.config;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CorsController {
-    @CrossOrigin(origins = {"http://43.203.38.124", "http://43.200.64.238"})
-    @GetMapping("/example")
-    public String example() {
-        return "Hello from Spring Boot!";
-    }
-}
+//package com.example.book_service.config;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class CorsController {
+//    @CrossOrigin(origins = {"http://43.203.38.124", "http://43.200.64.238"})
+//    @GetMapping("/example")
+//    public String example() {
+//        return "Hello from Spring Boot!";
+//    }
+//}
