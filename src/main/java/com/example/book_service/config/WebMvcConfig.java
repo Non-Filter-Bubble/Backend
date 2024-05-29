78505e6fc3dc59aae77a0861c9962205a8402646
Jihye <jihyeeee511@gmail.com>
2024-05-29 13:16:23 +0900

    WebMvcConfig 원래대로

--- src/main/java/com/example/book_service/config/WebMvcConfig.java
+++ src/main/java/com/example/book_service/config/WebMvcConfig.java
@@ -14,9 +14,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         // React CORS 설정
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 적용
-                .allowedOrigins("http://43.203.38.124") // react 애플리케이션의 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
-                .allowedHeaders("Authorization", "Content-Type", "Accept")
+                .allowedOrigins("http://43.203.38.124","http://43.200.64.238:8000") // react 애플리케이션의 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD") // 허용할 HTTP 메서드 설정
+                .allowedHeaders("*")
                 .allowCredentials(true);    // 자격 증명 허용 여부 설정
 
     }
