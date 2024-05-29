8281a4702fdd95116131dea2509785fa05048c68
Jihye <jihyeeee511@gmail.com>
2024-05-29 12:16:37 +0900

    token error -ing

--- src/main/java/com/example/book_service/config/WebMvcConfig.java
+++ src/main/java/com/example/book_service/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         // React CORS 설정
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 적용
-                .allowedOrigins("http://43.203.38.124", "http://43.200.64.238:8000") // react 애플리케이션의 도메인
+                .allowedOrigins("http://43.203.38.124") // react 애플리케이션의 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
                 .allowCredentials(true);    // 자격 증명 허용 여부 설정
 
